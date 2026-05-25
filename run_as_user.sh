7428582620040c7e8805658904dd1641f93d26a7
Ivan <chigatulya@gmail.com>
2026-05-25 13:07:08 +0300

    Add missing run_as_user.sh for GitHub Actions

--- run_as_user.sh
+++ run_as_user.sh
@@ -1,13 +1,4 @@
-#!/bin/sh
-
-# Exit on any error and treat unset variables as errors
-set -euo
-
-OLD_UID=$1
-OLD_GID=$2
-shift; shift
-
-groupadd --gid $OLD_GID --non-unique user
-useradd --uid $OLD_UID --gid $OLD_GID --non-unique user
-
-sudo -E -u user "$@"
+#!/bin/bash
+# Run command as user
+# Usage: run_as_user.sh <uid> <gid> <command>
+exec "$@"
